f6aeb56283a262d2bf4ad4171c076b1743377c2c
xzunun <jmg201098@gmail.com>
2023-01-19 20:47:04 -0600

    Bootstrap scaffolding added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><i class="fa-solid fa-house me-2"></i>{{ __('Dashboard') }}</div>
+                
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <i class="fa-solid fa-user"></i>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
