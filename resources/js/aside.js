dfd8e5389f1cab19af57a809b34648fdad1e7732
xzunun <jmg201098@gmail.com>
2023-01-29 11:48:44 -0600

    Funcionality added to nav and aside

--- resources/js/aside.js
+++ resources/js/aside.js
@@ -5,10 +5,12 @@ const openAside = document.querySelector('#open_aside');
 const closeAside = document.querySelector('#close_aside');
 
 const showAside = () => {
+    openAside.style.visibility = 'hidden';
     aside.style.visibility = 'visible';
     section.style.marginRight = '180px';
 }
 const hideAside = () => {
+    openAside.style.visibility = 'visible';
     aside.style.visibility = 'hidden';
     section.style.marginRight = 0;
 }
--- resources/views/layouts/scaffolding.blade.php
+++ resources/views/layouts/scaffolding.blade.php
@@ -1,22 +1,41 @@
 @extends('layouts.app')
 
 @section('app')
-<header class="border border-dark text-white">
+<header class="d-flex align-items-center px-2 border border-dark text-white sticky-top">
+    <div class="btn-open-nav p-2" id="open_nav">
+        <i class="fa-solid fa-bars fa-2x" title="Mostrar barra de navegación"></i>
+    </div>
     <span>Aquí va el header</span>
-    <span class="btn btn-primary" id="open_nav">Abrir nav</span>
-    <span class="btn btn-primary" id="close_nav">Cerrar nav</span>
 </header>
 <nav class="border border-dark text-white">
-    <span>Aquí va el nav</span>
+    <div class="btn-close-nav rounded-circle d-flex align-items-center ps-2" id="close_nav" title="Ocultar barra de navegación">
+        <i class="fa-solid fa-chevron-left"></i>
+    </div>
+    <div class="text-center my-4">
+        <img src="{{ asset('favicon.png') }}" class="shadow border rounded-circle" alt="Logo" width="120px" height="120px">
+        <div class="mt-2">
+            <span class="fs-4">{{ config('app.name') }}</span>
+            <small class="d-block text-secondary">Tlapalería</small>
+        </div>
+    </div>
+    <span>Aquí va nav</span>
 </nav>
 <main class="text-white">
     <section class="border border-dark">
         <span>Aquí va todo el contenido</span>
-        <span class="btn btn-warning" id="open_aside">Abrir aside</span>
-        <span class="btn btn-warning" id="close_aside">Cerrar aside</span>
+        @yield('scaffolding')
+        @yield('scaffolding')
+        @yield('scaffolding')
+        @yield('scaffolding')
         @yield('scaffolding')
     </section>
     <aside class="border border-dark">
+        <div class="btn-close-aside rounded-circle d-flex align-items-center pe-2 justify-content-end" id="close_aside" title="Ocultar barra lateral">
+            <i class="fa-solid fa-chevron-right"></i>
+        </div>
+        <div class="btn-open-aside rounded-circle d-flex align-items-center ps-2" id="open_aside" title="Mostrar barra lateral">
+            <i class="fa-solid fa-chevron-left"></i>
+        </div>
         <span>Aquí va el aside</span>
     </aside>
 </main>
