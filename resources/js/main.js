c175b35ede36ab28ec0edd2610e9320912d7a5fe
xzunun <jmg201098@gmail.com>
2023-02-06 09:15:33 -0600

    Some files modified

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,21 +1,7 @@
 const main = document.getElementsByTagName('main')[0];
 
-main?.offsetHeight > window.outerHeight ? [
+main?.offsetHeight > window.innerHeight ? [
     main?.classList.remove('fill-screen'),
 ] : [
     main?.classList.add('fill-screen'),
 ];
-
-// const resizeScreen = () => {
-//     main?.offsetHeight > window.innerHeight ? [
-//         main?.classList.remove('fill-screen'),
-//     ] : [
-//         main?.classList.add('fill-screen'),
-//     ];
-// }
-
-// setInterval(resizeScreen, 1000);
-
-// // window.addEventListener('load', resizeScreen);
-// // window.addEventListener('resize', resizeScreen);
-
